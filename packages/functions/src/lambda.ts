685e4c84eedf5a2c8fe874c652a9a3d840f7c454
tuokaikyle <kaituo@outlook.com>
2024-05-04 16:42:00 +1000

    simplified structure, change region

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from 'sst/node/api';
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hi, 世界. The time is ${new Date().toISOString()}`,
-  };
-});
